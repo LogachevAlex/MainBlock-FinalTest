476ba85b65eca0356c59d49468fb551b3ec91c98
Alexey Logachev <apelsin.alexey@gmail.com>
2024-01-20 14:46:30 +0400

    Добавил метод подсчета элементов длины до 3 элементов и начал писать матод создания второго массива

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string[] CreateArray(int length)
+string[] CreateArray(int length) //Создание массива. В качестве аргумента передается длина массива, пользователь заполняет каждый элемент массива типа string
 {   
     string[] array = new string[length];
     for (int i = 0; i < length; i++)
@@ -13,9 +13,9 @@ string[] CreateArray(int length)
     return array;
 }
 
-void PrintArray (string[] array)
+void PrintArray (string[] array) //Печать массива
 {   
-    Console.Write("Ваш массив: [ ");
+    Console.Write("Ваш массив: [");
     for (int i = 0; i < (array.Length - 1); i++)
     {
         Console.Write($"{array[i]}, ");
@@ -23,7 +23,30 @@ void PrintArray (string[] array)
     Console.Write($"{array[array.Length-1]}]");
 }
 
+int CountElementsUnder3Chars (string[] array) //Подсчет элементов в массиве длиной меньше 3
+{   
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+// string[] DuplicateArray(string[] array, int length) //Создание второго массива. В качестве аргумента передается исходный массив, возвращается новый массив только с элементами длины меньше 3. 
+// {
+//     string[] newArray = new string[length];
+//     for (int i = 0; i < length; i++)
+//     {
+//         Console.WriteLine($"Пожалуйста, введите {i+1} элемент массива");
+//         array[i] = Console.ReadLine()!;
+//     }
+//     return array;
+// }
+
 Console.WriteLine("Давайте создадим массив, введите желаемую длину массива: ");
 int userLength = int.Parse(Console.ReadLine()!);
 string[] userArray = CreateArray(userLength);
 PrintArray(userArray);
+int arrayCount = CountElementsUnder3Chars(userArray);
+
